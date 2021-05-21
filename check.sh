0995145c4ecbe57ce862de21e84efaf940a0e890
takashi.h <1samuraiboy@gmail.com>
2021-05-21 11:23:58 +0900

    add wifi on/off check

--- check.sh
+++ check.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
 AIRPORT_DIR="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources"
+
+## wifi on or off
+WIFI_CHECK=`${AIRPORT_DIR}/airport -I | grep "running" | awk '{print $2}'`
+
+if [[ ${WIFI_CHECK} != "running" ]]; then
+	echo OFF
+	exit 0
+fi
+
+## wifi encrypt check
 WIFI_ENCRYPT=`${AIRPORT_DIR}/airport -I | grep "link" | awk '{print $3}'`
-#echo ${WIFI_ENCRYPT}
-WIFI_ENCRYPT="wep"
+#WIFI_ENCRYPT="wep"
+
 if [[ ${WIFI_ENCRYPT} = "wpa2"* ]] || [[ ${WIFI_ENCRYPT} = "wpa3"* ]] ; then
-	echo OK
+	echo "WiFi is OK! reason:${WIFI_ENCRYPT}"
 else
 	#echo "あなたのネットワークは危険です"
 	user=$(ls -la /dev/console | cut -d " " -f 4)
