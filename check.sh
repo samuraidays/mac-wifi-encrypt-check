4c612ab360d515ea3bc0375d526f5c40199e6c81
12ban <12ban@12mac.local>
2022-11-15 14:25:25 +0800

    add comment

--- check.sh
+++ check.sh
@@ -6,7 +6,7 @@ logFile="/Library/Logs/TechSupport/WiFi-Encrypt-Check/check.log"
 mkdir -p /Library/Logs/TechSupport/WiFi-Encrypt-Check/
 touch ${logFile}
 
-## wifi on or off
+## wifi on or off check
 WIFI_CHECK=`${AIRPORT_DIR}/airport -I | grep "running" | awk '{print $2}'`
 
 if [[ ${WIFI_CHECK} != "running" ]]; then
@@ -19,10 +19,13 @@ WIFI_ENCRYPT=`${AIRPORT_DIR}/airport -I | grep "link" | awk '{print $3}'`
 #WIFI_ENCRYPT="wep"
 
 if [[ ${WIFI_ENCRYPT} = "wpa2"* ]] || [[ ${WIFI_ENCRYPT} = "wpa3"* ]] ; then
+	## if wpa2, wpa3
 	echo "$(date)" "WiFi is safety! reason:${WIFI_ENCRYPT}" >> "$logFile"
 else
+	## if another(e.g. wep)
 	user=$(ls -la /dev/console | cut -d " " -f 4)
 
+	## display alert window
 	userPrompt=$(sudo -u ${user} osascript -e '
 		display dialog "あなたの接続しているWiFiは危険です！" buttons {"OK"} default button 1 with title "WiFi Check"
 		set tmp to result
